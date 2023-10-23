d1e4544cb948d2cd6f8cb13d4a6f23a23e50d0d3
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-23 13:12:50 +0300

    int variable type added

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -2,7 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        //Value Types
+
+        //Console.WriteLine("Hello World!");
+
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 2147483647;
+        Console.WriteLine("number1 is {0}, number2 is {1}, number3 is {2}", number1, number2, number3);
         Console.ReadLine();
     }
 }
