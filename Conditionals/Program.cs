8bc6268f6e99334933de7cc60f17bb4257c354df
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-23 15:39:08 +0300

    if conditions added

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var number = 11;
+        if (number == 10) 
+        { 
+            Console.WriteLine("Number is 10"); 
+        }
+        else 
+        {
+            Console.WriteLine("Number is not 10");
+        }
+
+        Console.ReadLine(); 
+        
     }
 }
